ad4868acba84d10c6803a0f2d03d93dcce611817
Nathan John <nafanjohn@gmail.com>
2023-08-08 13:20:40 +0100

    cleaned up all errors and warnings showing in javascript console

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Footer from './components/footer'
 function App() {
   const [allCookies, setAllCookies] = useState([]);
   const [allRandomCookies, setAllRandomCookies] = useState([]);
-  const [errorMessage, setErrorMessage] = useState([]);
   const [newUser, setNewUser] = useState("");
   const [loginCookie, setLoginCookie] = useState('');
 
@@ -30,8 +29,6 @@ function App() {
             setAllCookies(data.hits);
         } catch (err) {
             console.log(err)
-            setErrorMessage("Data Not Found!")
-            console.log(errorMessage)
         }
     }
       fetchData()
@@ -41,7 +38,7 @@ useEffect(() => {
   const fetchData = async () => {
       try {
           // const response = await fetch(process.env.PUT_API_LINK_HERE);
-          const response = await fetch("https://api.edamam.com/api/recipes/v2?type=public&app_id=75e99c71&app_key=%2039b5b32d6e177083e0fbd7f397789d79&dishType=Biscuits%20and%20cookies&imageSize=REGULAR&random=truehttps://api.edamam.com/api/recipes/v2?type=public&app_id=75e99c71&app_key=%2039b5b32d6e177083e0fbd7f397789d79&dishType=Biscuits%20and%20cookies&imageSize=REGULAR&random=true");
+          const response = await fetch("https://api.edamam.com/api/recipes/v2?type=public&app_id=75e99c71&app_key=%2039b5b32d6e177083e0fbd7f397789d79&dishType=Biscuits%20and%20cookies&imageSize=REGULAR&random=true");
           if (!response.ok) {
               throw new Error(response.statusText)
           }
@@ -55,15 +52,13 @@ useEffect(() => {
             return {
                 cookieImage: cookie.recipe.images.REGULAR.url,
                 cookieName: cookie.recipe.label,
-                cookieUrl: cookie.recipe.url,
+                cookieUrl: cookie.recipe.shareAs,
                 cookieIngredients: ingredients
             }
         })
           setAllRandomCookies(cookieData);
       } catch (err) {
           console.log(err)
-          setErrorMessage("Data Not Found!")
-          console.log(errorMessage)
       }
   }
     fetchData()
@@ -72,7 +67,6 @@ useEffect(() => {
   return (
     <div className="App">
         <NavBar newUser={newUser} setNewUser={setNewUser} loginCookie={loginCookie} setLoginCookie={setLoginCookie} />
-        
         <Routes> 
           <Route index element={ <Home cookies={allRandomCookies} />} />
           <Route path="account" element={ <Account loginCookie={loginCookie} />} />
